5189d135e83ca7412cc7b5f019ac848427d3f082
reg222-stack <reg222register@gmail.com>
2020-05-18 01:22:56 +0400

    created events application frontend functional width auth0

---
+++ containers/PublicLayout/PublicNavigation.jsx
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import Button from 'components/Form'
+import { HeaderNavigation } from 'components/Header'
+import { NavigationItem, NavigationList } from 'components/Navigation'
+import { useFetchUser } from 'utils/user.js';
+
+const PublicNavigationContainer = ({ isOpen }) => {
+  const { user, loading } = useFetchUser()
+
+  return (
+    <HeaderNavigation isOpen={isOpen}>
+      <NavigationList className="NavigationList__left">
+        {user && !loading ? [
+          <NavigationItem href="/profile" key="profile">
+            Profile
+          </NavigationItem>,
+          <NavigationItem href="/event-listing" key="event-listing">
+            Event Listing
+          </NavigationItem>
+        ] : (
+          <NavigationItem href="/">
+            Home
+          </NavigationItem>
+        )}
+
+      </NavigationList>
+      <NavigationList className="NavigationList__right">
+        {user && !loading ? (
+          <NavigationItem href="/api/logout">
+            <Button
+              size="sm"
+              color="white">
+              Logout
+            </Button>
+          </NavigationItem>
+        ) : null}
+        {!user && !loading ? (
+          <NavigationItem href="/api/login">
+            <Button
+              size="sm"
+              color="primary">
+              Login
+            </Button>
+          </NavigationItem>
+        ) : null}
+      </NavigationList>
+    </HeaderNavigation>
+  )
+}
+
+PublicNavigationContainer.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+}
+
+export default PublicNavigationContainer
